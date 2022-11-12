82626cb9ec696275605ae0edb6d3204e23f2602e
jtdemey <johntdemey@live.com>
2022-11-11 23:37:35 -0500

    sveltin

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,45 +1,33 @@
 <script>
-  import svelteLogo from "./assets/svelte.svg";
-  import Counter from "./lib/Counter.svelte";
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank"
-      >SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+  <h1>Diseased Abyss</h1>
+  <hr />
+  <h2>A world of piracy for D&D 5e</h2>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  main {
+    width: 100%;
+    height: 100%;
+    margin: 1rem;
+    background: hsl(44, 56%, 80%);
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  h1 {
+    margin: 2rem 0;
+    padding: 0.4rem 0.8rem;
+    font-size: 2rem;
+    text-align: center;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+
+  hr {
+    width: 40%;
+    margin: auto;
   }
-  .read-the-docs {
-    color: #888;
+
+  h2 {
+    text-align: center;
   }
 </style>
